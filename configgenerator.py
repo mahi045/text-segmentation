68d2a3b2edf001fd8a3ec44d2d37f07883d410c9
Jaiaid <jaiaidmobin@gmail.com>
2020-11-23 15:36:02 +0600

    config and config generator updated to include wikicity and wikielements

--- configgenerator.py
+++ configgenerator.py
@@ -5,6 +5,8 @@ jsondata = {
     "choidataset": "/root/tempdir/text-segmentation/data/choi",
     "manifestodataset": "/root/tempdir/text-segmentation/data/manifesto",
     "wikidataset": "/home/omri/datasets/wikipedia/process_dump_r",
+    "wikicitydataset": "/root/tempdir/text_segmentation_bert/data/wikicity",
+    "wikielementdataset": "/root/tempdir/text_segmentation_bert/data/wikielement"
 }
 
 with open('config.json', 'w') as f:
